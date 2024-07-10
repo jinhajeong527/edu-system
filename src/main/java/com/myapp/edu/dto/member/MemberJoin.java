0b1126cc4296b7d8ac8794a8f33ce8efde1fb3c4
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-07-10 21:58:26 +0900

    refactor: MemberJoin @AllArgsConstructor 미사용 변경

--- src/main/java/com/myapp/edu/dto/member/MemberJoin.java
+++ src/main/java/com/myapp/edu/dto/member/MemberJoin.java
@@ -2,14 +2,20 @@ package com.myapp.edu.dto.member;
 
 import com.myapp.edu.domain.enums.Role;
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class MemberJoin {
+
     @NotBlank(message = "Username is required")
     private String username;
+
+    @Pattern(
+            regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            message = "Email should be valid"
+    )
+    @NotBlank(message = "Email is required")
+    private String email;
 
     @Size(min = 6, max = 10, message = "Password must be between 6 and 10 characters")
     @Pattern(
@@ -20,13 +26,6 @@ public class MemberJoin {
     private String password;
 
     @Pattern(
-            regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
-            message = "Email should be valid"
-    )
-    @NotBlank(message = "Email is required")
-    private String email;
-
-    @Pattern(
             regexp = "^(010|011|016|017|018|019)-\\d{3,4}-\\d{4}$",
             message = "Phone number must be in the format xxx-xxxx-xxxx or xxx-xxx-xxxx"
     )
@@ -34,4 +33,12 @@ public class MemberJoin {
 
     @NotNull(message = "Role is required")
     private Role role;
+
+    public MemberJoin(String username, String email, String password, String phoneNumber, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
 }
