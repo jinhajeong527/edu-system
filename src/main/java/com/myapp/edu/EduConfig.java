82a28a4e1fb09218f1c3677d1022ff6d9899ebf5
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-07-11 15:57:14 +0900

    refactor: WebMvcConfigurer 구현 설정 클래스와 JPA auditing 설정 분리

--- src/main/java/com/myapp/edu/EduConfig.java
+++ src/main/java/com/myapp/edu/EduConfig.java
@@ -1,8 +1,6 @@
 package com.myapp.edu;
 
-import com.myapp.edu.argumentresolver.InstructorArgumentResolver;
 import com.myapp.edu.auditor.AuditorAwareImpl;
-import com.myapp.edu.interceptor.LoginCheckInterceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
@@ -10,35 +8,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 
 @Configuration
 @EnableJpaAuditing
 @RequiredArgsConstructor
-public class EduConfig implements WebMvcConfigurer {
+public class EduConfig {
 
     private final HttpServletRequest httpServletRequest;
-
     @Bean
     public AuditorAware<String> auditorProvider() {
         return new AuditorAwareImpl(httpServletRequest);
     }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginCheckInterceptor())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/", "/auth/login", "/auth/logout", "/members", "/error");
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new InstructorArgumentResolver());
-    }
-
 }
---
+++ src/main/java/com/myapp/edu/WebConfig.java
@@ -0,0 +1,29 @@
+package com.myapp.edu;
+
+import com.myapp.edu.argumentresolver.InstructorArgumentResolver;
+import com.myapp.edu.interceptor.LoginCheckInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/auth/login", "/auth/logout", "/members", "/error", "/static/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new InstructorArgumentResolver());
+    }
+}
