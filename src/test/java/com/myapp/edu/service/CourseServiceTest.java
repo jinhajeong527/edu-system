a28a570f8dc45d09558df446e0b2c01013a6c99d
jinha <77036027+jinhajeong527@users.noreply.github.com>
2024-07-13 09:25:21 +0900

    test: 정원 내의 강의만 수강 신청 성공하도록 테스트 코드 추가

--- src/test/java/com/myapp/edu/service/CourseServiceTest.java
+++ src/test/java/com/myapp/edu/service/CourseServiceTest.java
@@ -3,6 +3,7 @@ package com.myapp.edu.service;
 import com.myapp.edu.domain.Course;
 import com.myapp.edu.domain.Member;
 import com.myapp.edu.domain.MemberCourse;
+import com.myapp.edu.dto.course.CourseApply;
 import com.myapp.edu.dto.course.CourseResponse;
 import com.myapp.edu.enums.Role;
 import com.myapp.edu.dto.course.CourseSave;
@@ -10,6 +11,7 @@ import com.myapp.edu.repository.CourseRepository;
 import com.myapp.edu.repository.MemberCourseRepository;
 import com.myapp.edu.repository.MemberRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
@@ -18,7 +20,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
@@ -48,16 +52,19 @@ class CourseServiceTest {
 
         // when & then
         Assertions.assertThrows(IllegalStateException.class, () -> courseService.create(instructorEmail, courseSave));
+        verify(memberRepository).findByEmail(instructorEmail);
     }
 
     @Test
     void shouldCreateCourse() {
         // given
         String instructorEmail = "testuser@example.com";
-        Member member = new Member("testuser", instructorEmail,
+        Member member = new Member(1L, "testuser", instructorEmail,
                 "password123", "010-1234-5678", Role.INSTRUCTOR);
+
         CourseSave courseSave = new CourseSave("course1", 5L, new BigDecimal(100000));
-        Course course = new Course(courseSave.getTitle(), courseSave.getPrice(), courseSave.getMaxCapacity());
+
+        Course course = new Course(1L, courseSave.getTitle(), courseSave.getPrice(), courseSave.getMaxCapacity());
 
         given(memberRepository.findByEmail(instructorEmail)).willReturn(Optional.of(member));
         given(courseRepository.save(ArgumentMatchers.any(Course.class))).willReturn(course);
@@ -76,6 +83,40 @@ class CourseServiceTest {
         verify(memberCourseRepository).save(ArgumentMatchers.any(MemberCourse.class));
     }
 
+    @Test
+    @DisplayName("정원 초과한 강의는 수강 신청 강의 목록에 미포함 된다.")
+    void shouldEnrollMemberInAvailableCourses() {
+        // given
+        Member member1 = new Member(1L, "testuser1", "testuser1@example.com",
+                "password1", "010-1111-5678", Role.STUDENT);
+        Course course1 = new Course(1L, "course1", new BigDecimal(100000), 1L);
+
+        course1.enrollMember(member1); // course1에 수강생 1명 증가
+
+        Member member2 = new Member(2L, "testuser2", "testuser2@example.com",
+                "password2", "010-2222-5678", Role.STUDENT);
+        Course course2 = new Course(2L, "course2", new BigDecimal(200000), 2L);
+        course2.enrollMember(member2); // course2에 수강생 1명 증가
 
 
+        String email3 = "testuser3@example.com";
+        Member member3 = new Member(3L, "testuser3", "testuser3@example.com",
+                "password3", "010-3333-5678", Role.STUDENT);
+
+        CourseApply courseApply = new CourseApply(Set.of(course1.getId(), course2.getId()));
+
+        given(memberRepository.findByEmail(email3)).willReturn(Optional.of(member3));
+        given(courseRepository.findAllByIdIn(courseApply.getCourseIds())).willReturn(List.of(course1, course2));
+
+        // when
+        List<CourseResponse> courses = courseService.applyForCourses(email3, courseApply);
+
+        // then
+        assertThat(courses).hasSize(1);
+        assertThat(courses.get(0).getCurrentEnrollment()).isEqualTo(2);
+        assertThat(courses.get(0).getTitle()).isEqualTo("course2");
+
+        verify(memberRepository).findByEmail(email3);
+        verify(courseRepository).findAllByIdIn(courseApply.getCourseIds());
+    }
 }
